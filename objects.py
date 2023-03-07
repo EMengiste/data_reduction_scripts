f24e7a43cd3d99ae27687e4debad5993d8ae11b1
EMengiste <32231387+EMengiste@users.noreply.github.com>
2023-03-06 20:24:28 -0600

    I have the thing done

--- objects.py
+++ objects.py
@@ -49,6 +49,7 @@ class fepx_sim:
         #
         #
         ##@ Status checks
+        self.sim=""
         self.results_dir=os.listdir(path)
         self.completed = "post.report" in self.results_dir
         self.has_config = "simulation.config" in self.results_dir
@@ -380,50 +381,68 @@ def avg(arr):
 #
 home="/media/etmengiste/acmelabpc2_2TB/DATA/jobs/aps/spring_2023/slip_study_rerun/"
 home="/media/schmid_2tb_1/etmengiste/files/slip_study_rerun/"
+home="/Users/ezramengiste/Documents/neper_fepx_gui/the_sims"
 
-simulations = os.listdir(home)
-simulations.sort()
-pprint(simulations)
-for i in simulations:
-    if i !="common_files":
-        for domain in ["Elongated","Cube"]:
-            sim= fepx_sim("name",path=home+i+"/"+domain)
-            try:
-                sim.post_process(options ="neper -S . -reselset slip,crss,stress,sliprate")
-            except:
-                print("lol it broke")
-end =time.time()
 
-print("I took "+str((end-start)/60)+" minutes")
-exit(0)
 #sim.post_process(options ="neper -S . -reselset slip,crss,stress,sliprate")
-sim_iso= fepx_sim("name",path=home+"isotropic/Cube")
+sim_iso= fepx_sim("name",path=home+"/1_uniaxial")
 #sim_iso.post_process(options ="neper -S . -reselset slip,crss,stress,sliprate")
 
+slip_systems = ["$(01-1)[111]$",                
+                "$(10-1)[111]$",
+                "$(1-10)[111]$",
 
-sample_num = 2000
+                "$(011)[11-1]$",
+                "$(101)[11-1]$",
+                "$(1-10)[11-1]$",
+
+                "$(011)[1-11]$",
+                "$(10-1)[1-11]$",
+                "$(110)[1-11]$",
+
+                "$(01-1)[1-1-1]$",
+                "$(101)[1-1-1]$",
+                "$(110)[1-1-1]$"]
+sim_iso.post_process()
+val =sim_iso.sim["**general"].split()
+sample_num = int(int(val[2])/10)
 start = 0
+bin_size = 10
 ids = np.arange(start,start+sample_num,1)
-
-slip_iso =  [normailze(sim_iso.get_output("slip",step="28",res="elsets",ids=ids)[str(i)],absolute=True) for i in ids]
+step = "1"
+res ="elts"
+slip_iso =  [normailze(sim_iso.get_output("slip",step=step,res=res,ids=ids)[str(i)],absolute=True) for i in ids]
+stress_iso = [normailze(sim_iso.get_output("stress",step=step,res=res,ids=ids)[str(i)]) for i in ids]
 slips_iso = {}
 
 baseline = float(sim_iso.material_parameters["g_0"][0].split("d")[0])
-
 del sim_iso
-
-fig = plt.figure()
+comps=np.arange(6)
+fig = plt.figure(figsize=[60,20])
+ax_stress = fig.add_subplot(2,7,13)
 axies = []
 for i in range(len(slip_iso[0])):
     slips =[]
-    ax= fig.add_subplot(2,6,i+1)
+    ax= fig.add_subplot(2,7,i+1)
     for id in ids:
         slips.append(slip_iso[id][i])
+        if i == 0:
+            stress_fake_1 =  [ 0.3*float(i) for i in stress_iso[id]] 
+            plotting_space([stress_fake_1,stress_iso[id]],axis=ax_stress)
     slips_iso[str(i)] = slips
-    ax.hist(slips,bins=10,color="blue",edgecolor="red",alpha=0.2)
-    ax.set_ylim([0,int(sample_num*0.7)])
+    #print(slips)
+    slip_fake_1 = [  abs(float(i-0.002)) for i in slips] 
+    ax.hist(slip_fake_1,bins=bin_size,color="red",edgecolor="k",alpha=0.2)
+    ax.hist(slips,bins=bin_size,color="blue",edgecolor="k",alpha=0.2)
+    ax.set_title(slip_systems[i])
+    ax.set_ylim([0,int(sample_num)])
     axies.append(ax)
 
+
+plt.tight_layout()
+plt.subplots_adjust(left=0.062, bottom=0.08, right=0.9,top=0.948, wspace=0.162,hspace=.126)
+plt.show()
+exit(0)
 for sim in ["046","050","075"]:
     sim= fepx_sim("name",path=home+sim+"/Cube")
     #sim.post_process(options ="neper -S . -reselset slip,crss,stress,sliprate")
